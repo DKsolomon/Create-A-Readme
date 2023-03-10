be51fe1a309c537f9ee6c4e072937c610232eb74
DKsolomon <DKSolomon8135@gmail.com>
2023-03-09 16:54:00 -0800

    wrote functions to create readme, and initialize app

--- index.js
+++ index.js
@@ -57,10 +57,25 @@ const questions = [
     },];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Created Readme');
+        }
+    });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        writeToFile('README.md', generateMarkdown(data));
+    });
+}
 
 // Function call to initialize app
 init();
